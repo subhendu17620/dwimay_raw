b576a6072343bf3e94c5842178bfeff733b7eeb3
subhendu17620 <subhendu17620@gmail.com>
2020-01-12 15:43:19 +0530

    slider

--- events page/slider/script.js
+++ events page/slider/script.js
@@ -46,32 +46,27 @@
     /* Hero Case study images */
 
     $(".slide-buttons li:nth-child(1)").on("mouseenter", function() {
-      $(".slide-buttons li.active").removeClass("active");
-      $(".hero-center-section.show").removeClass("show");
+      $(".hero-center-section").removeClass("show");
       $(".hero-center-section:nth-child(1)").addClass("show");
       $(".slide-buttons li:nth-child(1)").addClass("active");
     });
     $(".slide-buttons li:nth-child(2)").on("mouseenter", function() {
-      $(".slide-buttons li.active").removeClass("active");
-      $(".hero-center-section.show").removeClass("show");
+      $(".hero-center-section").removeClass("show");
       $(".hero-center-section:nth-child(2)").addClass("show");
       $(".slide-buttons li:nth-child(2)").addClass("active");
     });
     $(".slide-buttons li:nth-child(3)").on("mouseenter", function() {
-      $(".slide-buttons li.active").removeClass("active");
-      $(".hero-center-section.show").removeClass("show");
+      $(".hero-center-section").removeClass("show");
       $(".hero-center-section:nth-child(3)").addClass("show");
       $(".slide-buttons li:nth-child(3)").addClass("active");
     });
     $(".slide-buttons li:nth-child(4)").on("mouseenter", function() {
-      $(".slide-buttons li.active").removeClass("active");
-      $(".hero-center-section.show").removeClass("show");
+      $(".hero-center-section").removeClass("show");
       $(".hero-center-section:nth-child(4)").addClass("show");
       $(".slide-buttons li:nth-child(4)").addClass("active");
     });
     $(".slide-buttons li:nth-child(5)").on("mouseenter", function() {
-      $(".slide-buttons li.active").removeClass("active");
-      $(".hero-center-section.show").removeClass("show");
+      $(".hero-center-section").removeClass("show");
       $(".hero-center-section:nth-child(5)").addClass("show");
       $(".slide-buttons li:nth-child(5)").addClass("active");
     });
